7185957947ce86c6f46b0efb1eeb0fa6dc0b6ecf
Syarif Ys <5yarif.y.s@gmail.com>
2022-12-03 12:47:40 +0800

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Contact name="Bryan Curry" phone="882235436577" />
+      <Contact name="Steve Cox" phone="08155464612" />
+      <Contact name="Mark Alexanderson" phone="0852311231" />
     </div>
   );
 }
