c46e03f6ed41a2197561e897b90715d0dd938f9a
Syarif Ys <5yarif.y.s@gmail.com>
2022-12-04 19:19:29 +0800

    add list

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,29 @@ import React from "react";
 import Contact from "./components/Contact";
 
 function App() {
+  const contactItems = [
+    {
+      id: 1,
+      name: "Bryan Curry",
+      phone: "082235436577",
+    },
+    {
+      id: 2,
+      name: "Steve Cox",
+      phone: "08155464612",
+    },
+    {
+      id: 3,
+      name: "Mark Alexanderson",
+      phone: "0852311231",
+    },
+  ];
+
   return (
     <div>
-      <Contact name="Bryan Currey" phone="082235436577" />
-      <Contact name="Steve Cox" phone="08155464612" />
-      <Contact name="Mark Alexanderson" phone="0852311231" />
+      {contactItems.map((item) => (
+        <Contact key={item.id} name={item.name} phone={item.phone} />
+      ))}
     </div>
   );
 }
